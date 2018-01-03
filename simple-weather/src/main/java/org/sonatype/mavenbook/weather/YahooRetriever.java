0dfca8368cefe17c16c2b08cd3a7c620ec0a4e63
Juan Sicilia <siciliajuan@gmail.com>
2018-01-03 19:06:09 +0100

    Add build dependencias to build the jar with all dependencies

--- simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
+++ simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
@@ -15,8 +15,7 @@ public class YahooRetriever {
 
     public InputStream retrieve(String zipcode) throws Exception {
         log.info( "Retrieving Weather Data" );
-        String url = "http://weather.yahooapis.com/forecastrss?p="
-                + zipcode;
+        String url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
         URLConnection conn = new URL(url).openConnection();
         return conn.getInputStream();
     }
